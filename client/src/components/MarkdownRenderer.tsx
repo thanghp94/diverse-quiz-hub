fd7bb9180321ce6656e907642496196c3810ab0a
thanghuynh15 <42147426-thanghuynh15@users.noreply.replit.com>
2025-06-17 09:57:04 +0000

    Restored to '954c26f21a408053f11afb6662178c97059482d7'
    Replit-Restored-To: 954c26f21a408053f11afb6662178c97059482d7

--- client/src/components/MarkdownRenderer.tsx
+++ client/src/components/MarkdownRenderer.tsx
@@ -13,7 +13,7 @@ export const MarkdownRenderer = ({
   children: string;
   className?: string;
 }) => (
-  <div className={`prose prose-blue dark:prose-invert max-w-none whitespace-pre-wrap font-sans prose-li:my-0 prose-li:mb-0 prose-li:mt-0 prose-li:leading-relaxed prose-li:pl-0 prose-p:my-0 prose-p:mb-0 prose-p:leading-relaxed prose-ul:my-0 prose-ul:mb-0 prose-ul:pl-1 prose-ul:mt-0 prose-ol:my-0 prose-ol:mb-0 prose-ol:pl-1 prose-ol:mt-0 ${className}`} style={{ lineHeight: '1.1' }}>
+  <div className={`prose prose-blue dark:prose-invert max-w-none whitespace-pre-wrap font-sans prose-li:my-0 prose-li:mb-0 prose-li:mt-0 prose-li:leading-none prose-li:pl-0 prose-p:my-0 prose-p:mb-0 prose-p:leading-tight prose-ul:my-0 prose-ul:mb-0 prose-ul:pl-1 prose-ul:mt-0 prose-ol:my-0 prose-ol:mb-0 prose-ol:pl-1 prose-ol:mt-0 ${className}`} style={{ lineHeight: '1.1' }}>
     <ReactMarkdown>{children}</ReactMarkdown>
   </div>
 );
